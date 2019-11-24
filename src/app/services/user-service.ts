c0a240c02b150d38e0ed2e71d540a786d3c990ce
Chandrasekar Arunachalam <kingring23@hotmail.com>
2019-11-24 08:47:35 +0530

    Added logger service.

--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -3,25 +3,28 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { User } from '../shared/User';
+import { LoggerService } from './logger.service';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+    providedIn: 'root'
+})
 export class UserService {
     private url = 'api/users';
     httpOptions = { headers: new HttpHeaders({'content-type': 'application/json'}) };
-    constructor(private readonly http: HttpClient) {
+    constructor(private readonly http: HttpClient, private loggerService: LoggerService) {
     }
     getUsers(): Observable<any> {
         return this.http.get(this.url)
             .pipe(
-                tap(() => console.log('Fetching users.')),
-                catchError(error => console.log)
+                tap(() => this.loggerService.info('Fetching users list')),
+                catchError(error => this.loggerService.info)
             );
     }
     addUser(user: User): Observable<any> {
         return this.http.post(this.url, user, this.httpOptions)
             .pipe(
-                tap(() => console.log('Adding user ...')),
-                catchError(error => console.log)
+                tap(() => this.loggerService.info('Adding new user')),
+                catchError(error => this.loggerService.error)
             );
     }
 }
